35b93b7737c99597fae0c10ed65b6374e67d408b
Yasser <yasdle@outlook.com>
2023-06-08 03:10:26 +0800

    first commit of customers controller(get and post)

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,34 @@
+using Detailing.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace detailing.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CustomerController : ControllerBase
+{
+    private static IEnumerable<Customer> customers = new List<Customer>{
+        new Customer{
+            FirstName = "Yas",
+            LastName = "dle",
+        },
+        new Customer{
+            FirstName = "Hiba",
+            LastName = "sh"
+        }
+    };
+
+    [HttpGet]
+    public IEnumerable<Customer> Get()
+    {
+        return customers;
+    }
+
+    [HttpPost]
+    public static Customer Post(Customer newCustomer)
+    {
+        customers.ToList().Add(newCustomer);
+        return newCustomer;
+    }
+}
+
