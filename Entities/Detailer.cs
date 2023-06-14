785d271ea4a68ce4024ed68ab392d2979cfbaa4c
Yasser <yasdle@outlook.com>
2023-06-14 21:14:26 +0800

    Added more properties to Detailer class

--- Entities/Detailer.cs
+++ Entities/Detailer.cs
@@ -3,5 +3,11 @@ namespace Detailing.Entities
     public class Detailer : Person
     {
         public bool HasBusiness { get; set; }
+
+        public bool DetailsExterior { get; set; }
+
+        public bool DetailsInterior { get; set; }
+
+        public bool IsMobile { get; set; }
     }
 }
