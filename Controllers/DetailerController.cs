69b74eac90c59b8fea9d19f4d1e2415aa953a072
Yasser <yasdle@outlook.com>
2023-06-14 22:10:03 +0800

    Added Detailer controller

---
+++ Controllers/DetailerController.cs
@@ -0,0 +1,55 @@
+using Detailing.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Detailing.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class DetailerController : ControllerBase
+{
+    private static IEnumerable<Detailer> detailers = new List<Detailer>{
+        new Detailer{
+            Id = 101,
+            FirstName = "Yas",
+            LastName = "dle",
+        },
+        new Detailer{
+            Id = 1001,
+            FirstName = "Hiba",
+            LastName = "sh"
+        }
+    };
+
+    [HttpGet]
+    public IEnumerable<Detailer> Get()
+    {
+        return detailers;
+    }
+
+    [HttpGet("{detailerId:int}")]
+    public ActionResult<Detailer> Get(int detailerId)
+    {
+        var detailer = detailers.FirstOrDefault(d => d.Id == detailerId);
+        if (detailer == default)
+        {
+            return NotFound();
+        }
+
+        return detailer;
+    }
+
+    [HttpPost]
+    public ActionResult Post(Detailer newDetailer)
+    {
+        try
+        {
+            detailers.ToList().Add(newDetailer);
+            return Created(string.Empty, newDetailer);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest();
+        }
+    }
+}
+
