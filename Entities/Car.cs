3c31ca272d4fa0f8d25644eb9e551d7e052ebcec
Yasser <yasdle@outlook.com>
2023-06-14 21:23:47 +0800

    Added Car entity and as a property

---
+++ Entities/Car.cs
@@ -0,0 +1,19 @@
+namespace Detailing.Entities
+{
+    public class Car
+    {
+        public int Id { get; set; }
+
+        public string Manufacturer { get; set; }
+
+        public string Model { get; set; }
+
+        public string Year { get; set; }
+
+        public string Color { get; set; }
+
+        public Person Owner { get; set; }
+
+        public DateTime LastDetailed { get; set; }
+    }
+}
--- Entities/Customer.cs
+++ Entities/Customer.cs
@@ -2,6 +2,7 @@ namespace Detailing.Entities
 {
     public class Customer : Person
     {
+        public IEnumerable<Car> Cars { get; set; }
 
     }
 }
