eadcf29d2d94140ded31b334977e354f11079bc8
Yasser <yasdle@outlook.com>
2023-07-20 21:39:16 +0800

    Added CarMapper class

---
+++ Mappers/CarMapper.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Data;
+using Detailing.Interfaces;
+using Detailing.Entities;
+
+namespace Detailing.Mappers
+{
+    public class CarMapper : IDataMapper<Car>
+    {
+        public Car MapToEntity(DataRow row)
+        {
+            var lastDetailed = row["LastDetailingDate"].ToString();
+            var car =  new Car
+            {
+                Id = Convert.ToInt32(row["Id"]),
+                Manufacturer = row["Manufacturer"].ToString(),
+                Model = row["Model"].ToString(),
+                Year = row["Year"].ToString(),
+                Color = row["Color"].ToString(),
+                LastDetailed = string.IsNullOrEmpty(lastDetailed) ? default : Convert.ToDateTime(lastDetailed),
+            };
+            
+            return car;
+        }
+    }
+}
