a16cd22cf21a4ae9bc69aeeff689dad6a9d6e13d
Yasser <yasdle@outlook.com>
2023-07-22 01:56:39 +0800

    Implemented ExecuteNonQueryStoredProcedure

--- Helpers/DatabaseHelper.cs
+++ Helpers/DatabaseHelper.cs
@@ -8,12 +8,35 @@ namespace Detailing.Helpers
     public class DatabaseHelper<T> : IDatabaseHelper<T>
     {
         private static string connectionString = "Server=localhost;User ID=root;Password=password;Database=detailing";
-        
-        public bool ExecuteNonQueryStoredProcedure(string storedProcedureName)
-        {
-            throw new NotImplementedException();
-        }
 
+        public void ExecuteNonQueryStoredProcedure(string storedProcedureName, MySqlParameter[] parameters)
+        {
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                using (var command = new MySqlCommand(storedProcedureName, connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddRange(parameters);
+                    try
+                    {
+                        Console.WriteLine("Openning sql connection");
+                        connection.Open();
+                        command.ExecuteNonQuery();
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Exception thrown: {ex.Message}");
+                    }
+                    finally
+                    {
+                        connection.Close();
+                        Console.WriteLine("Closing sql connection");
+                    }
+                }
+            }
+        }
+        
         public DataTable ExecuteQueryStoredProcedure(string storedProcedureName)
         {
             var dtResult = new DataTable();
@@ -35,7 +58,7 @@ namespace Detailing.Helpers
                     }
                     catch (Exception ex)
                     {
-                        Console.WriteLine($"Exception thrown: {ex.Message}" );
+                        Console.WriteLine($"Exception thrown: {ex.Message}");
                     }
                     finally
                     {
@@ -46,7 +69,7 @@ namespace Detailing.Helpers
             }
 
             return dtResult;
-        
+
         }
 
         public T ExecuteScalarQueryStoredProcedure(string storedProcedureName)
