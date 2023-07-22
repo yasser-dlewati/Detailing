420550e6fb1bc428f2e1048efd099b9648b2d7dd
Yasser <yasdle@outlook.com>
2023-07-22 10:54:00 +0800

    Implemented CRUD from CarController

--- Controllers/CarController.cs
+++ Controllers/CarController.cs
@@ -9,26 +9,42 @@ namespace Detailing.Controllers;
 public class CarController : ControllerBase
 {
     private readonly CarRepository carRepository = new CarRepository();
-    
+
     [HttpGet]
-    public IEnumerable<Car> Get()
+    public IActionResult Get()
     {
         var cars = carRepository.GetAll();
-        return cars;
+        return Ok(cars);
     }
 
     [HttpGet("{CarId:int}")]
-    public ActionResult<Car> Get(int CarId)
+    public IActionResult Get(int CarId)
     {
-        return null;
+        var car = carRepository.GetSingleById(CarId);
+        return car != null ? Ok(car) : NotFound();
     }
 
 
     [HttpPost]
-    public ActionResult<Car> Post(Car newCar)
+    public IActionResult Post([FromBody] Car newCar)
     {
-        carRepository.TryInsert(newCar);
-        return Ok(newCar);
+        var isCarInserted = carRepository.TryInsert(newCar, out var carId);
+        newCar.Id = carId;
+        return isCarInserted ? CreatedAtAction(nameof(Get), newCar) : BadRequest();
+    }
+
+    [HttpPut]
+    public IActionResult Put([FromBody] Car carToUpdate)
+    {
+        var isCarUpdated = carRepository.TryUpdate(carToUpdate);
+        return isCarUpdated ? Ok(carToUpdate) : BadRequest();
+    }
+
+     [HttpDelete("{carId:int}")]
+    public IActionResult Delete(int carId)
+    {
+        var isCarDeleted = carRepository.TryDelete(carId);
+        return isCarDeleted ? NoContent() : BadRequest();
     }
 }
 
