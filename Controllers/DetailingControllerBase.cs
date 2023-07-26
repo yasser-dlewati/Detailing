d9cc94ee681af80fb9d6d6adeaf08d8d59559cfe
Yasser <yasdle@outlook.com>
2023-07-26 22:14:22 +0800

    Made DetailingControl more generic

--- Controllers/DetailingControllerBase.cs
+++ Controllers/DetailingControllerBase.cs
@@ -5,7 +5,7 @@ namespace Detailing.Controllers
 {
 
     [ApiController]
-    public abstract class DetailingControllerBase<T> : ControllerBase
+    public abstract class DetailingControllerBase<T> : ControllerBase where T : IModel
     {
         private readonly IConfiguration _config;
         private IDatabaseService _dbservice;
@@ -16,6 +16,43 @@ namespace Detailing.Controllers
             _config = config;
             dbservice.ConnectionString = config.GetConnectionString("localMysqlConnectionstring");
             _dbservice = dbservice;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var modelsList = _repoService.GetAll();
+            return Ok(modelsList);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult Get(int id)
+        {
+            var model = _repoService.GetSingleById(id);
+            return model != null ? Ok(model) : NotFound();
+        }
+
+
+        [HttpPost]
+        public IActionResult Post([FromBody] T newModel)
+        {
+            var isModelInserted = _repoService.TryInsert(newModel, out var id);
+            newModel.Id = id;
+            return isModelInserted ? CreatedAtAction(nameof(Get), newModel) : BadRequest();
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] T modelToUpdate)
+        {
+            var isModelUpdated = _repoService.TryUpdate(modelToUpdate);
+            return isModelUpdated ? Ok(modelToUpdate) : BadRequest();
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            var isModelDeleted = _repoService.TryDelete(id);
+            return isModelDeleted ? NoContent() : BadRequest();
         }
 
     }
