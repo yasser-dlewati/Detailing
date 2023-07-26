4614ddd6d346e158320fec0f041830ce27b4f268
Yasser <yasdle@outlook.com>
2023-07-26 23:19:51 +0800

    Implement CarController to follow the generic changes

--- Controllers/CarController.cs
+++ Controllers/CarController.cs
@@ -2,6 +2,7 @@ using Detailing.Models;
 using Detailing.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Detailing.Interfaces;
+using Detailing.Mappers;
 
 namespace Detailing.Controllers;
 
@@ -10,44 +11,6 @@ public class CarController : DetailingControllerBase<Car>
 {
     public CarController(IConfiguration config, IDatabaseService dbService) : base(config, dbService)
     {
-        _repoService = new CarRepository(dbService);
-    }
-
-    [HttpGet]
-    public IActionResult Get()
-    {
-        var cars = _repoService.GetAll();
-        return Ok(cars);
-    }
-
-    [HttpGet("{CarId:int}")]
-    public IActionResult Get(int CarId)
-    {
-        var car = _repoService.GetSingleById(CarId);
-        return car != null ? Ok(car) : NotFound();
-    }
-
-
-    [HttpPost]
-    public IActionResult Post([FromBody] Car newCar)
-    {
-        var isCarInserted = _repoService.TryInsert(newCar, out var carId);
-        newCar.Id = carId;
-        return isCarInserted ? CreatedAtAction(nameof(Get), newCar) : BadRequest();
-    }
-
-    [HttpPut]
-    public IActionResult Put([FromBody] Car carToUpdate)
-    {
-        var isCarUpdated = _repoService.TryUpdate(carToUpdate);
-        return isCarUpdated ? Ok(carToUpdate) : BadRequest();
-    }
-
-    [HttpDelete("{carId:int}")]
-    public IActionResult Delete(int carId)
-    {
-        var isCarDeleted = _repoService.TryDelete(carId);
-        return isCarDeleted ? NoContent() : BadRequest();
+        _repoService = new CarRepository(dbService, new CarMapper());
     }
 }
-
