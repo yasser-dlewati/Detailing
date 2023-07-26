e5f952ca5a562aed62e038f0f627d8ef7addcd03
Yasser <yasdle@outlook.com>
2023-07-26 23:19:24 +0800

    Made CarRepo extends RepoService

--- Repositories/CarRepository.cs
+++ Repositories/CarRepository.cs
@@ -2,105 +2,31 @@ using System.Data;
 using Detailing.Interfaces;
 using Detailing.Models;
 using Detailing.Mappers;
+using Detailing.Services;
+using System.Data.Common;
 
 namespace Detailing.Repositories
 {
-    public class CarRepository : IEntityRepository<Car>
+    public class CarRepository : RepositorySerivce<Car>
     {
-        private readonly IDatabaseService _dbService;
-        private readonly CarMapper carMapper = new CarMapper();
-
-        public CarRepository(IDatabaseService dbService)
+        public CarRepository(IDatabaseService dbService, CarMapper carMapper) : base (dbService, carMapper)
         {
-            _dbService = dbService;
+            
         }
 
-        public IEnumerable<Car> GetAll()
+        protected override string SelectAllStoredProcedureName => "sp_car_select_all";
+
+        protected override string SelectByIdStoredProcedureName => "sp_car_select_by_Id";
+
+        protected override string InsertStoredProcedureName => "sp_car_insert";
+
+        protected override string UpdateStoredProcedureName => "sp_car_update";
+
+        protected override string DeleteByIdStoredProcedureName => "sp_car_delete_by_Id";
+
+        public override IDbDataParameter[] GetDbParameters(Car car)
         {
-            var dtCar = _dbService.ExecuteQueryStoredProcedure("sp_car_select_all");
-            var cars = new List<Car>();
-            for (var i = 0; i < dtCar.Rows.Count; i++)
-            {
-                var car = carMapper.MapToModel(dtCar.Rows[i]);
-                cars.Add(car);
-            }
-            return cars;
-        }
-
-        public Car GetSingleById(int carId)
-        {
-            try
-            {
-                if (carId > 0)
-                {
-                    var spParameters = new IDbDataParameter[]
-                    {
-                        new DatabaseParameter("Id", carId),
-                    };
-
-                    var dtcar = _dbService.ExecuteQueryStoredProcedure("sp_car_select_by_Id", spParameters);
-                    var car = carMapper.MapToModel(dtcar.Rows[0]);
-                    return car;
-                }
-            }
-            catch (Exception)
-            {
-
-            }
-
-            return null;
-        }
-
-        public bool TryDelete(int carId)
-        {
-            try
-            {
-                if (carId > 0)
-                {
-                    var spParameters = new IDbDataParameter[]
-                    {
-                        new DatabaseParameter("Id", carId),
-                    };
-                    var rowsAffectd = _dbService.ExecuteNonQueryStoredProcedure("sp_car_delete_by_Id", spParameters);
-                    return rowsAffectd == 1;
-                }
-            }
-            catch (Exception)
-            {
-            }
-
-            return false;
-        }
-
-        public bool TryInsert(Car car, out int insertedId)
-        {
-            try
-            {
-                var spParameters = new IDbDataParameter[]
-                {
-                new DatabaseParameter("Manufacturer", car.Manufacturer),
-                new DatabaseParameter("Model", car.Model),
-                new DatabaseParameter("Year", car.Year),
-                new DatabaseParameter("Color", car.Color),
-                new DatabaseParameter("OwnerId", car.Owner.Id),
-                new DatabaseParameter("LastDetailingDate", car.LastDetailed),
-                };
-                insertedId = _dbService.ExecuteNonQueryStoredProcedure("sp_car_insert", spParameters);
-                return insertedId > 0;
-            }
-            catch (Exception ex)
-            {
-            }
-
-            insertedId = 0;
-            return false;
-        }
-
-        public bool TryUpdate(Car car)
-        {
-            try
-            {
-                var spParameters = new IDbDataParameter[]
+             var dbParamerters = new IDbDataParameter[]
                 {
                 new DatabaseParameter("Id", car.Id),
                 new DatabaseParameter("Manufacturer", car.Manufacturer),
@@ -110,14 +36,9 @@ namespace Detailing.Repositories
                 new DatabaseParameter("OwnerId", car.Owner.Id),
                 new DatabaseParameter("LastDetailingDate", car.LastDetailed),
                 };
-                var rowsAffectd = _dbService.ExecuteNonQueryStoredProcedure("sp_car_update", spParameters);
-                return rowsAffectd == 1;
-            }
-            catch (Exception ex)
-            {
-            }
 
-            return false;
+                return dbParamerters;
         }
+
     }
 }
