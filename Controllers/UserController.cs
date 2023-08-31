3889d5409e386dccee7cb9f0aa63608e34ffb22c
Yasser <yasdle@outlook.com>
2023-08-31 18:36:09 +0800

    Wrapped UserController with Auth & Role

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,8 +1,10 @@
 using Detailing.Models;
 using Detailing.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Detailing.Controllers;
 
+[Authorize(Roles ="Admin")]
 public class UserController : DetailingControllerBase<User>
 {
     public UserController(IConfiguration config, IDatabaseService dbService, IModelManager<User> manager) : base(config, dbService, manager)
