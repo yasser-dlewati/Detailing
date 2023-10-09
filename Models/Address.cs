fc12158b1667f79f1b07e94d42dde0cdf465d3f0
Yasser <yasdle@outlook.com>
2023-10-09 23:41:56 +0800

    Added long&lat props to Address.cs

--- Models/Address.cs
+++ Models/Address.cs
@@ -10,6 +10,8 @@ namespace Detailing.Models
         public string ZipCode { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
+        public double Longitude {get; set;}
+        public double Latitude { get; set; }
 
     }
 }
