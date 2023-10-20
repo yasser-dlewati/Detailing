2c2554666ca559f4af31d41f8acf1967a6a40797
Yasser <yasdle@outlook.com>
2023-10-20 21:43:23 +0800

    Added Api version

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Detailing.Controllers;
 
 [ApiController]
-[Route("api/user/[controller]")]
+[Route("api/v{version:apiVersion}/user/[controller]")]
 public class CustomerController : DetailingControllerBase<Customer>
 {
     public CustomerController(IConfiguration config, IDatabaseService dbService, IModelManager<Customer> manager) : base(config, dbService, manager)
--- Controllers/DetailerController.cs
+++ Controllers/DetailerController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Detailing.Controllers;
 
 [ApiController]
-[Route("api/user/[controller]")]
+[Route("api/v{version:apiVersion}/user/[controller]")]
 public class DetailerController : DetailingControllerBase<Detailer>
 {
     public DetailerController(IConfiguration config, IDatabaseService dbservice, IModelManager<Detailer> manager) : base(config, dbservice, manager)
