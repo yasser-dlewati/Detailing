e2581b4ac46426d6e0859a6f84063cbb252cdd5f
Yasser <yasdle@outlook.com>
2023-10-28 22:14:38 +0800

    Added attrs to Csr.cs

--- Models/Car.cs
+++ Models/Car.cs
@@ -1,21 +1,29 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using Detailing.Interfaces;
 
 namespace Detailing.Models
 {
     public class Car: IModel
     {
+        [Column("CarId")]
         public int Id { get; set; }
 
+        [Column("Manufacturer")]
         public string Manufacturer { get; set; }
 
+        [Column("Model")]
         public string Model { get; set; }
 
+        [Column("Year")]
         public string Year { get; set; }
 
+        [Column("Color")]
         public string Color { get; set; }
 
+        [Column("UserId")]
         public int OwnerId { get; set; }
 
+        [Column("LastDetailingDate")]
         public DateTime? LastDetailed { get; set; }
     }
 }
