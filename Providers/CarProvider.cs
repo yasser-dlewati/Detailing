bf748e13e53a69ef65372cee6a0831e537e92d33
Yasser <yasdle@outlook.com>
2023-10-28 22:17:04 +0800

    Used parent implementation in carProvider.cs

--- Providers/CarProvider.cs
+++ Providers/CarProvider.cs
@@ -1,9 +1,5 @@
-using System.Data;
 using Detailing.Interfaces;
 using Detailing.Models;
-using Detailing.Mappers;
-using Detailing.Services;
-using System.Data.Common;
 
 namespace Detailing.Providers
 {
@@ -23,22 +19,5 @@ namespace Detailing.Providers
         public override string UpdateStoredProcedureName => "sp_car_update";
 
         public override string DeleteByIdStoredProcedureName => "sp_car_delete_by_Id";
-
-        public override IDbDataParameter[] GetDbParameters(Car car)
-        {
-             var dbParamerters = new IDbDataParameter[]
-                {
-                new DatabaseParameter("Id", car.Id),
-                new DatabaseParameter("Manufacturer", car.Manufacturer),
-                new DatabaseParameter("Model", car.Model),
-                new DatabaseParameter("Year", car.Year),
-                new DatabaseParameter("Color", car.Color),
-                new DatabaseParameter("OwnerId", car.OwnerId),
-                new DatabaseParameter("LastDetailingDate", car.LastDetailed),
-                };
-
-                return dbParamerters;
-        }
-
     }
 }
