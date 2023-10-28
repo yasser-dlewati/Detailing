f91255d6341fc13f0967bab98d1d99f21222fcdd
Yasser <yasdle@outlook.com>
2023-10-28 22:11:09 +0800

    Added more props & attrs to Business.cs

--- Models/Business.cs
+++ Models/Business.cs
@@ -1,17 +1,37 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using Detailing.Interfaces;
 
 namespace Detailing.Models
 {
     public class Business : IModel
     {
+        [Column("BusinessId")]
         public int Id { get; set; }
-        public Detailer Owner { get; set; }
-        public IEnumerable<Detailer>? Crew { get; set; }
+
+        [Column("DetailerId")]
+        public int OwnerId { get; set; }
+
+        [Column("BusinessName")]
+        public string BusinessName { get; set; }
+
         public Address Address { get; set; }
+
+        [Column("Website")]
         public string Website { get; set; }
+
+        [Column("SocialMedia")]
         public string SocialMedia { get; set; }
+
+        [Column("Email")]
+        public string Email { get; set; }
+
+        [Column("Phone")]
         public string Phone { get; set; }
+        
+        [Column("Established")]
         public DateTime Established { get; set; }
+
+        [Column("Description")]
         public string Description { get; set; }
     }
 }
