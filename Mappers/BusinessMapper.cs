64a0ca953c96fe38a033636cbdfd62f738bc6ec6
Yasser <yasdle@outlook.com>
2023-10-28 22:10:14 +0800

    Implemented MapToModel in BusinessMapper

--- Mappers/BusinessMapper.cs
+++ Mappers/BusinessMapper.cs
@@ -2,13 +2,25 @@ using System.Data;
 using Detailing.Interfaces;
 using Detailing.Models;
 
-namespace Detailing.Mappers
+namespace Detailing.Mappers;
+public class BusinessMapper : IDataMapper<Business>
 {
-    public class BusinessMapper : IDataMapper<Business>
+    public Business MapToModel(DataRow row)
     {
-        public Business MapToModel(DataRow row)
+        var addressMapper = new AddressMapper();
+        var address = addressMapper.MapToModel(row);
+        var business = new Business
         {
-            throw new NotImplementedException();
-        }
+            Id = Convert.ToInt32(row["BusinessId"]),
+            Address = address,
+            Description = row["Description"].ToString(),
+            Established = DateTime.Parse(row["Established"].ToString()),
+            Phone = row["Phone"].ToString(),
+            OwnerId = Convert.ToInt32(row["DetailerId"]),
+            SocialMedia = row["SocialMedia"].ToString(),
+            Website = row["Website"].ToString(),
+        };
+
+        return business;
     }
 }
