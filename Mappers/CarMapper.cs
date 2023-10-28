4d67ec3892e3c0fe18e4165f07bbb2e0d7fbe59c
Yasser <yasdle@outlook.com>
2023-10-28 22:15:54 +0800

    Fixed columns names in carMapper.cs

--- Mappers/CarMapper.cs
+++ Mappers/CarMapper.cs
@@ -8,15 +8,16 @@ namespace Detailing.Mappers
     {
         public Car MapToModel(DataRow row)
         {
+            
             var lastDetailed = row["LastDetailingDate"].ToString();
             var car =  new Car
             {
-                Id = Convert.ToInt32(row["Id"]),
+                Id = Convert.ToInt32(row["CarId"]),
                 Manufacturer = row["Manufacturer"].ToString(),
                 Model = row["Model"].ToString(),
                 Year = row["Year"].ToString(),
                 Color = row["Color"].ToString(),
-                OwnerId = Convert.ToInt32(row["OwnerId"]),
+                OwnerId = Convert.ToInt32(row["UserId"]),
                 LastDetailed = string.IsNullOrEmpty(lastDetailed) ? null : Convert.ToDateTime(lastDetailed),
             };
             
