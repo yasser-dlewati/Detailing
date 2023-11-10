b77fa40fa5b13812ef9d7b2a710676f9be5827de
Yasser <yasdle@outlook.com>
2023-11-10 19:42:14 +0800

    Fixed runtime error because of using Async sufffix

--- Program.cs
+++ Program.cs
@@ -14,7 +14,10 @@ using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.SuppressAsyncSuffixInActionNames = false;
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
