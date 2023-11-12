a0fa10c6e88e495d70837ff72859d9f69eb80124
Yasser <yasdle@outlook.com>
2023-11-12 23:08:00 +0800

    Authoriswed car controller

--- Controllers/CarController.cs
+++ Controllers/CarController.cs
@@ -1,7 +1,9 @@
 using Detailing.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Detailing.Controllers;
 
+[Authorize(Roles ="Customer")]
 public class CarController : DetailingControllerBase<Car>
 {
     public CarController(IServiceProvider provider) : base(provider)
