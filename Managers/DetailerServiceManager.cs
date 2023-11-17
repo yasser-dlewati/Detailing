1f32d9c4bdea9bd5ab320e183d4aa837f92648de
Yasser <yasdle@outlook.com>
2023-11-17 16:07:27 +0800

    Added DetailerServic endpoints

--- Managers/DetailerServiceManager.cs
+++ Managers/DetailerServiceManager.cs
@@ -16,4 +16,14 @@ public class DetailerServiceManager : BaseManager<DetailerService>
     {
         return await (_provider as DetailerServiceProvider).GetDetailerServicesAsync(detailerId);
     }
+
+    public async Task<DetailerService> GetDetailerServiceAsync(int detailerId, int serviceId)
+    {
+        return await (_provider as DetailerServiceProvider).GetDetailerServiceAsync(detailerId, serviceId);
+    }
+
+    public async new Task<bool> TryDeleteAsync(int  detailerId, int serviceId)
+    {
+        return await (_provider as DetailerServiceProvider).TryDeleteAsync(detailerId, serviceId);
+    }
 }
--- Providers/DetailerServiceProvider.cs
+++ Providers/DetailerServiceProvider.cs
@@ -1,5 +1,6 @@
 using System.Data;
 using Detailing.Interfaces;
+using Detailing.Mappers;
 using Detailing.Models;
 
 namespace Detailing.Providers;
@@ -16,7 +17,7 @@ public class DetailerServiceProvider : BaseProvider<DetailerService>
 
     public override string SelectAllStoredProcedureName => throw new NotImplementedException();
 
-    public override string SelectByIdStoredProcedureName => "sp_Service_select_by_Id";
+    public override string SelectByIdStoredProcedureName => "sp_Service_select_by_Id_DetailerId";
 
     public override string InsertStoredProcedureName => "sp_Service_insert";
 
@@ -38,24 +39,77 @@ public class DetailerServiceProvider : BaseProvider<DetailerService>
 
     public async Task<IEnumerable<DetailerService>> GetDetailerServicesAsync(int detailerId)
     {
-        var spParameter = new IDbDataParameter[]
+        try
         {
-            new DatabaseParameter("UserId", detailerId)
-        };
-
-        var dt = await _dbService.ExecuteQueryStoredProcedureAsync(SelectByDetailerIdStoredProcedureName, spParameter);
-        if(dt!=null && dt.Rows!= null && dt.Rows.Count>0)
-        {
-            var services = new List<DetailerService>();
-            foreach(DataRow dtRow in dt.Rows)
+            var spParameter = new IDbDataParameter[]
             {
-                var service = _mapper.MapToModel(dtRow);
-                services.Add(service);
-            }
+            new DatabaseParameter("UserId", detailerId)
+            };
 
-            return services;
+            var dt = await _dbService.ExecuteQueryStoredProcedureAsync(SelectByDetailerIdStoredProcedureName, spParameter);
+            if (dt != null && dt.Rows != null && dt.Rows.Count > 0)
+            {
+                var services = new List<DetailerService>();
+                foreach (DataRow dtRow in dt.Rows)
+                {
+                    var service = _mapper.MapToModel(dtRow);
+                    services.Add(service);
+                }
+
+                return services;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Exception thrown at GetDetailerServicesAsync({detailerId}): {ex.Message}");
         }
 
         return Enumerable.Empty<DetailerService>();
+    }
+
+    public async Task<DetailerService> GetDetailerServiceAsync(int detailerId, int serviceId)
+    {
+        try
+        {
+            var spParameter = new IDbDataParameter[]
+{
+            new DatabaseParameter("UserId", detailerId),
+            new DatabaseParameter("ServiceId", serviceId),
+};
+
+            var dt = await _dbService.ExecuteQueryStoredProcedureAsync(SelectByIdStoredProcedureName, spParameter);
+            if (dt != null && dt.Rows != null && dt.Rows.Count == 0)
+            {
+                var service = _mapper.MapToModel(dt.Rows[0]);
+                return service;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Exception thrown at GetDetailerServiceAsync({detailerId},{serviceId}): {ex.Message}");
+        }
+
+        return null;
+    }
+
+    public async Task<bool> TryDeleteAsync(int detailerId, int serviceId)
+    {
+        try
+        {
+            var spParameter = new IDbDataParameter[]
+            {
+            new DatabaseParameter("UserId", detailerId),
+            new DatabaseParameter("ServiceId", serviceId),
+            };
+
+            var rowsAffected = await _dbService.ExecuteNonQueryStoredProcedureAsync(SelectByIdStoredProcedureName, spParameter);
+            return rowsAffected == 1;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Exception thrown at TryDeleteAsync({detailerId},{serviceId}): {ex.Message}");
+        }
+
+        return false;
     }
 }
