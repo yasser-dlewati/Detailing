5f5ac71445b0e83cc242de448a13ed5f0548ef98
Yasser <yasdle@outlook.com>
2023-11-17 15:32:56 +0800

    Removed unnessacery props in detaile & job models

--- Models/Detailer.cs
+++ Models/Detailer.cs
@@ -4,16 +4,8 @@ namespace Detailing.Models
 {
     public class Detailer : User
     {
-        public int DetailerId {get; set;}
-
-        public bool? HasBusiness { get; set; }
-
-        public bool? DetailsExterior { get; set; }
-
-        public bool? DetailsInterior { get; set; }
-
-        public bool? IsMobile { get; set; }
-
         public override UserType Role => UserType.Detailer;
+
+        public IEnumerable<DetailerService> Services { get; set; }
     }
 }
